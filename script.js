e85d90c124ae0226b2cb04cf39e135b56950fb56
Alek-Grujic <alek.grujic001@outlook.com>
2026-02-02 12:22:29 +0100

    feat: add new workout objects to workouts array

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ class Workout {
 class App {
   #map;
   #mapEvent;
+  #workouts = [];
 
   constructor() {
     this._getPosition();
@@ -91,16 +92,15 @@ class App {
     const type = inputType.value;
     const distance = +inputDistance.value;
     const duration = +inputDuration.value;
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
 
     // check if data is valid
 
     // if workout running, create running object
     if (type === "running") {
       const cadence = +inputCadence.value;
-      if (
-        type === "running" &&
-        !hasValue(inputDistance, inputDuration, inputCadence)
-      ) {
+      if (!hasValue(inputDistance, inputDuration, inputCadence)) {
         return alert("Please fill in all required fields!");
       }
       if (
@@ -110,17 +110,17 @@ class App {
 
         !validInputs(distance, duration, cadence) ||
         !allPositive(distance, duration, cadence)
-      )
+      ) {
         return alert("Inputs have to be positive numbers!");
+      }
+
+      workout = new Running([lat, lng], distance, duration, cadence);
     }
 
     // if workout cycling, create cycling object
     if (type === "cycling") {
       const elevation = +inputElevation.value;
-      if (
-        type === "cycling" &&
-        !hasValue(inputDistance, inputDuration, inputElevation)
-      ) {
+      if (!hasValue(inputDistance, inputDuration, inputElevation)) {
         return alert("Please fill in all required fields!");
       }
       if (
@@ -130,11 +130,14 @@ class App {
 
         !validInputs(distance, duration, elevation) ||
         !allPositive(distance, duration)
-      )
+      ) {
         return alert("Inputs have to be positive numbers!");
+      }
+      workout = new Cycling([lat, lng], distance, duration, elevation);
     }
 
     // add new object to workout array
+    this.#workouts.push(workout);
 
     // render workout on map as marker
 
@@ -145,8 +148,6 @@ class App {
       inputCadence.value =
       inputElevation.value =
         "";
-
-    const { lat, lng } = this.#mapEvent.latlng;
 
     L.marker([lat, lng])
       .addTo(this.#map)
